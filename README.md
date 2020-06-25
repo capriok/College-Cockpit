98ef60963809c960202b204b81f454dfa62804ca
Kyle Caprio <capriok7@yahoo.com>
2020-06-24 18:05:16 -0700

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+<p align="center"><img width="250" src="https://i.imgur.com/F3IZ2B6.png" alt="Cockpit Logo"></p>
+
+<h1 align="center">College Cockpit</h1>
+
+College Cockpit is a central hub for everything associated with my college career.
+
+## License
+This project is licensed under the terms of the [MIT license](/LICENSE)
---
+++ public/electron.js
@@ -0,0 +1,37 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require('path');
+const isDev = require('electron-is-dev');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 2000,
+    minWidth: 1000,
+    height: 1400,
+    minHeight: 1200,
+  });
+  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+  if (isDev) {
+    // Open the DevTools..
+    // mainWindow.webContents.openDevTools();
+  }
+  mainWindow.on('closed', () => mainWindow = null);
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
